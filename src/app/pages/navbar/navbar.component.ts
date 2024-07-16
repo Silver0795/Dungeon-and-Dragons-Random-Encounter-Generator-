1b132a0235a3758b26b9bba4a2c673d65766da74
AndreaC <andreacrinella@icloud.com>
2024-07-16 21:11:32 +0200

    Added copy to clipboard and snackbar autoclosed

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSnackBar} from '@angular/material/snack-bar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule,MatButtonModule],
+  imports: [MatToolbarModule,MatIconModule,MatButtonModule,ClipboardModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+
+  _snackBar : MatSnackBar = inject(MatSnackBar);
+
+  link = window.location.href;
+
+
   scrollToTop(){
     window.scrollTo(0,0);
+  }
+
+  openSnackBar() {
+    this._snackBar.open("Copied to clipboard", "", {duration: 3000})
   }
 
 }
